6730edf6f5062573734c899c338c3286153c3ea8
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-02-25 15:06:39 +0200

    search added

--- src/App.js
+++ src/App.js
@@ -2,64 +2,84 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
-const getLocalStorage =()=>{
-  let list = localStorage.getItem('list')
-  if (list){
-    return JSON.parse(localStorage.getItem('list'));
-  }
-  else return [];
-}
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else return [];
+};
 
 function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchValue = React.useRef("");
+  const [searchName, setSearchName] = useState("");
+  const [filteredList,setFilteredList]=useState(list)
 
-  
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      showAlert(true, 'danger', 'please enter value')
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
-      setList(list.map((item)=>{
-        if (item.id===editId){
-          return {...item, title:name}
-          return item
-        }
-        
-      }))
-      setName('')
-        setEditId(null)
-        setIsEditing(false)
-        showAlert(true, 'success', 'value changed')
+      setList(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
+            return item;
+          }
+        })
+      );
+      setName("");
+      setEditId(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
+      setFilteredList(list)
     } else {
-      showAlert(true, 'success', 'item added to the list')
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
+      setFilteredList([...list, newItem])
     }
   };
-  const showAlert =(show= false,type='',msg='')=>{
-    setAlert({show,type,msg})
-  }
-  const clearlist =()=>{
-    showAlert(true, 'danger', 'Empty List')
-    setList([])
-  }
-  const removeItem=(id)=>{
-    showAlert(true, 'danger', 'item removed')
-    setList(list.filter((item)=>item.id!==id))
-  }
-  const editItem=(id)=>{
-    const specificItem = list.find((item)=>item.id===id)
-    setIsEditing(true)
-    setEditId(id)
-    setName(specificItem.title)
-  }
-    useEffect(() => {
-    localStorage.setItem('list', JSON.stringify(list));
+  const searchTasks = () => {
+    setSearchTerm(searchValue.current.value);
+    let newList = list.filter((specificItem) => {
+      return specificItem.title
+        .toLowerCase()
+        .startsWith(searchValue.current.value);
+    });
+    setFilteredList(newList);
+
+    if (searchValue.current.value.length < 1) {
+      setFilteredList(list);
+    }
+  };
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+  const clearlist = () => {
+    showAlert(true, "danger", "Empty List");
+    setList([]);
+    setFilteredList([])
+  };
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+    setFilteredList(list)
+  };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  };
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
   }, [list]);
 
   return (
@@ -67,11 +87,12 @@ function App() {
       <form className="task-form" onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} list={list} removeAlert={showAlert} />}
         <h3>task manager app</h3>
+        <p>Add task</p>
         <div className="form-control">
           <input
             type="text"
             className="task"
-            placeholder="e.g.eggs"
+            placeholder="search by task name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
@@ -81,10 +102,25 @@ function App() {
           </button>
         </div>
       </form>
+      <form className="task-form" onSubmit={handleSubmit}>
+        <p>Search task</p>
+        <div className="form-control">
+          <input
+            type="text"
+            className="task"
+            placeholder="task name"
+            ref={searchValue}
+            onChange={searchTasks}
+          />
+        </div>
+      </form>
+
       {list.length > 0 && (
         <div className="task-container">
-          <List items={list} removeItem={removeItem}  editItem={editItem} />
-          <button className="clear-btn" onClick={clearlist}>clear items</button>
+          <List items={filteredList} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearlist}>
+            clear items
+          </button>
         </div>
       )}
     </section>
